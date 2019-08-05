9f5a6ecdbe5dd0d51dad6f742346638bd7a9a274
Brenden Matthews <brenden@diddyinc.com>
2019-08-05 18:31:24 -0400

    Adjust fee calculation.

--- src/service.rs
+++ src/service.rs
@@ -20,8 +20,8 @@ use crate::stripe_client;
 static MAX_PAYMENT_AMOUNT: i32 = 97_099_969;
 
 // Umpyre fees
-static UMPYRE_SEND_FEE: f64 = 0.15; // 15%
-static UMPYRE_READ_FEE: f64 = 0.15; // 15%
+static UMPYRE_MESSAGE_SEND_FEE: f64 = 0.15; // 15%
+static UMPYRE_MESSAGE_READ_FEE: f64 = 0.15; // 15%
 
 lazy_static! {
     static ref PAYMENT_ADDED: prometheus::HistogramVec = {
@@ -503,7 +503,7 @@ impl BeanCounter {
         let client_uuid_to = Uuid::parse_str(&request.client_id_to)?;
 
         let payment_cents = request.payment_cents;
-        let fee_cents = (f64::from(payment_cents) * UMPYRE_SEND_FEE).floor() as i32;
+        let fee_cents = (f64::from(payment_cents) * UMPYRE_MESSAGE_SEND_FEE).round() as i32;
         let total_amount = payment_cents + fee_cents;
 
         // Any payment over this amount will never go through
@@ -601,7 +601,7 @@ impl BeanCounter {
 
                 // If there's a valid payment, perform settlement
                 let fee_amount =
-                    (f64::from(payment.payment_cents) * UMPYRE_READ_FEE).floor() as i32;
+                    (f64::from(payment.payment_cents) * UMPYRE_MESSAGE_READ_FEE).round() as i32;
                 let payment_amount_after_fee = payment.payment_cents - fee_amount;
 
                 // Add TX from umpyre cash account to recipient
