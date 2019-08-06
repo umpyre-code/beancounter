a1c3d4f2e178654d94eb2fde81390682e66fdab4
Brenden Matthews <brenden@diddyinc.com>
2019-08-06 15:19:53 -0400

    Cache cargo registry.

--- build.sh
+++ build.sh
@@ -28,6 +28,7 @@ gcloud auth activate-service-account --key-file=$SCCACHE_GCS_KEY_PATH
 gsutil -m -q rsync -r gs://umpyre-sccache/sccache $SCCACHE_DIR || true
 mkdir -p target
 gsutil -m -q rsync -r gs://umpyre-sccache/$REPO_NAME/target target || true
+gsutil -m -q rsync -r gs://umpyre-sccache/$REPO_NAME/cargo-registry $CARGO_HOME/registry || true
 
 sccache -s
 
@@ -38,3 +39,4 @@ sccache -s
 
 gsutil -m -q rsync -r $SCCACHE_DIR gs://umpyre-sccache/sccache || true
 gsutil -m -q rsync -r target gs://umpyre-sccache/$REPO_NAME/target || true
+gsutil -m -q rsync -r $CARGO_HOME/registry gs://umpyre-sccache/$REPO_NAME/cargo-registry || true
