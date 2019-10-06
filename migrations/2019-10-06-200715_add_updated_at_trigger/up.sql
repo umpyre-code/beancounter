d99ee1589bebc39d6a39062bf03f9b44c221e0e4
Brenden Matthews <brenden@diddyinc.com>
2019-10-06 16:13:02 -0400

    Add updated_at trigger migration.

---
+++ migrations/2019-10-06-200715_add_updated_at_trigger/up.sql
@@ -0,0 +1,6 @@
+SELECT diesel_manage_updated_at('balances');
+SELECT diesel_manage_updated_at('payments');
+SELECT diesel_manage_updated_at('stripe_charges');
+SELECT diesel_manage_updated_at('stripe_connect_accounts');
+SELECT diesel_manage_updated_at('stripe_connect_transfers');
+SELECT diesel_manage_updated_at('transactions');
